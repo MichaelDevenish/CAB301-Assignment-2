189c12218345d5e6447e2621a93d2f4fc419e343
Michael Devenish <michael@michaeldevenish.com>
2017-04-26 23:23:26 +1000

    removed test code

--- main.cpp
+++ main.cpp
@@ -1,23 +1,11 @@
 #include <iostream>
 #include <MinDistance.h>
 #include <MinDistance2.h>
-#include <FileOutput.h>
 
 using namespace std;
 
 int main()
 {
-    int setSize = 50;
-    int testSize = 10;
-    double outputArray[setSize][testSize];
-    for (int i = 0; i < setSize; i++){
-        for(int e = 0; e < testSize; e++){
-            outputArray[i][e] = i+e;
-        }
-    }
-
-    FileOutput::writeCSV(setSize,testSize,outputArray);
-
     MinDistance dist = MinDistance();
 
     cout << "Hello world!" << endl;
