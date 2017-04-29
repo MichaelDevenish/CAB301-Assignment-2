9eeed0fbde06e3e8810d17a776f0b7302479b782
Michael Devenish <michael@michaeldevenish.com>
2017-04-29 18:39:49 +1000

    implemented algorithm 2

--- include/MinDistance2.h
+++ include/MinDistance2.h
@@ -1,14 +1,14 @@
 #ifndef MINDISTANCE2_H
 #define MINDISTANCE2_H
-
+#include <math.h>
 
 class MinDistance2
 {
     public:
+        static int MinDistanceTwo(double input[], int inputSize);
+    protected:
         MinDistance2();
         virtual ~MinDistance2();
-
-    protected:
 
     private:
 };
--- src/MinDistance2.cpp
+++ src/MinDistance2.cpp
@@ -9,3 +9,16 @@ MinDistance2::~MinDistance2()
 {
     //dtor
 }
+
+int MinDistance2::MinDistanceTwo(double input[], int inputSize){
+    int dmin = INT_MAX;
+    for (int i = 0; i < inputSize-2; i++){
+        for (int j = i+1; j < inputSize-1; j++ ){
+            int temp = round(input[i]-input[j]); //basic operation
+            if(temp < dmin){
+                dmin = temp;
+            }
+        }
+    }
+    return dmin;
+}
